c844a6cddfd07369480e9f728197c6afc6d190aa
Artem Yefremov <artem.biruck@gmail.com>
2025-03-04 13:26:29 +0200

    Add files via upload

---
+++ server.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE penan_punkat;
+USE penan_punkat; 
+
+ CREATE TABLE hero_text (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT NOT NULL
+);
+INSERT INTO hero_text (content) VALUES ('Tervetuloa Penan Punkat verkkosivustolle!');
